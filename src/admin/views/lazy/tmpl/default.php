28d6519a796d69b021bf88a7c18e7b9130ed57cd
C-Lodder <lodder15@hotmail.co.uk>
2020-05-04 10:06:22 +0100

    Couple of improvements

--- src/admin/views/lazy/tmpl/default.php
+++ src/admin/views/lazy/tmpl/default.php
@@ -3,8 +3,8 @@ defined('_JEXEC') or die('Restricted Access');
 
 use Joomla\CMS\HTML\HTMLHelper;
 
-HTMLHelper::_('script', 'com_addlazyloading/app.esm.js', array('version' => 'auto', 'relative' => true), array('type' => 'module', 'defer' => true));
-HTMLHelper::_('script', 'com_addlazyloading/app.es5.js', array('version' => 'auto', 'relative' => true), array('nomodule' => '', 'defer' => true));
+HTMLHelper::_('script', 'com_addlazyloading/app.esm.js', ['version' => 'auto', 'relative' => true), ['type' => 'module', 'defer' => true]];
+HTMLHelper::_('script', 'com_addlazyloading/app.es5.js', ['version' => 'auto', 'relative' => true), ['nomodule' => '', 'defer' => true]];
 
 echo '<h1 id="lazyLoadingTitle">⬆️⬆️⬆️ Click on the Update Articles button to initialise the process</h1>'
      . '<div id="lazyLoadingApp"></div>'
